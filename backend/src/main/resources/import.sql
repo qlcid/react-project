0152800b16cbae0497ee775dccc6d87e28e49cc5
220585 <ysyes@shinsgea.com>
2022-09-29 09:30:04 +0900

    db: import.sql 추가

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO `react`.`language` (`language_id`, `language`) VALUES ('1', 'JAVA');
+INSERT INTO `react`.`language` (`language_id`, `language`) VALUES ('2', 'Python');
+INSERT INTO `react`.`language` (`language_id`, `language`) VALUES ('3', 'Javascript');
+INSERT INTO `react`.`language` (`language_id`, `language`) VALUES ('4', 'C++');
+INSERT INTO `react`.`language` (`language_id`, `language`) VALUES ('5', 'C#');
