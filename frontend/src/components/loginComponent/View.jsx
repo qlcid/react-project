60cc073c3dca5db842d6108752e5b092a4643370
yjszz <actafool@naver.com>
2022-09-29 15:55:18 +0900

    login

--- frontend/src/components/loginComponent/View.jsx
+++ frontend/src/components/loginComponent/View.jsx
@@ -1,34 +1,43 @@
 import React, { useState } from "react";
 import RegistContainer from "../registComponent/View";
 import RegistPopup from "../registComponent/Regist";
+import "./style.css";
 
-const LoginContainer = (props) => {
-  const { onClose } = props;
+const LoginContainer = ({ onChange, user, onSubmit, onClose }) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div class="input-group mb-3">
-      <input
-        type="text"
-        class="form-control"
-        placeholder="ID"
-        aria-label="login-id"
-      />
-      <input
-        type="password"
-        class="form-control"
-        placeholder="PASSWORD"
-        aria-label="login-password"
-      />
-      <div class="input-group-append">
+    <div className="loginContainerBox">
+      {/* <form onSubmit={onSubmit}> */}
+      <div>
+        <input
+          name="userId"
+          placeholder="ID"
+          value={user.userId}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <input
+          name="password"
+          type="password"
+          placeholder="PASSWORD"
+          value={user.password}
+          onChange={onChange}
+        />
+      </div>
+      <div>
         <button
-          class="btn btn-outline-secondary"
+          id="loginSubmitBtn"
+          className="loginSubmitBtn"
+          //type="submit"
+          value="로그인"
           type="button"
-          id="btn_login"
           // 임시로 모달닫는 동작만 추가
-          onClick={() => {
-            onClose(false);
-          }}
+          // onClick={() => {
+          //   onClose(false);
+          // }}
+          onClick={onSubmit}
         >
           로그인
         </button>
@@ -36,6 +45,7 @@ const LoginContainer = (props) => {
           class="btn btn-outline-secondary"
           type="button"
           id="btn_regist"
+          className="btn_regist"
           onClick={() => {
             setOpen(true);
           }}
@@ -49,6 +59,7 @@ const LoginContainer = (props) => {
           }}
         />
       </div>
+      {/* </form> */}
     </div>
   );
 };
--- frontend/src/components/loginComponent/containers/LoginPopup.jsx
+++ frontend/src/components/loginComponent/containers/LoginPopup.jsx
@@ -1,28 +1,87 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import Dialog from "@mui/material/Dialog";
 import LoginContainer from "../View";
 import PropTypes from "prop-types";
 
-const LoginPopup = (props) => {
-  const { onClose, open, dark } = props;
-  const handleClose = () => {
-    onClose();
-  };
-  return (
-    <Dialog
-      onClose={handleClose}
-      open={open}
-      PaperProps={{
-        style: {
-          backgroundColor: "transparent",
-          boxShadow: "none",
-        },
-      }}
-    >
-      <LoginContainer dark={dark} onClose={onClose} hide={false} />
-    </Dialog>
-  );
-};
+class LoginPopup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        userId: "",
+        password: "",
+      },
+    };
+
+    this.submitLogin = this.submitLogin.bind(this);
+    this.loginhandleChange = this.loginhandleChange.bind(this);
+  }
+
+  loginhandleChange(event) {
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
+
+    this.setState({
+      user,
+    });
+  }
+
+  submitLogin(user) {
+    var user = {
+      userId: this.state.user.userId,
+      password: this.state.user.password,
+    };
+    var params = {
+      userId: user.userId,
+      password: user.password,
+    };
+
+    axios
+      .post("/api/user/login", params) //서버
+      .then((res) => {
+        if (res.status === 200) {
+          alert("로그인 성공");
+        } else {
+          alert("로그인 실패");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const { onClose, open, dark } = this.props;
+    const handleClose = () => {
+      onClose();
+    };
+    return (
+      <Dialog
+        onClose={handleClose}
+        open={open}
+        PaperProps={{
+          style: {
+            backgroundColor: "transparent",
+            boxShadow: "none",
+          },
+        }}
+      >
+        <LoginContainer
+          dark={dark}
+          onChange={this.loginhandleChange}
+          onSubmit={this.submitLogin}
+          onClose={onClose}
+          hide={false}
+          user={this.state.user}
+        />
+      </Dialog>
+    );
+  }
+}
+
 LoginPopup.propTypes = {
   onClose: PropTypes.func,
   open: PropTypes.bool,
